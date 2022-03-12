b44f6e6f63d2b29382e4b6d5af41bcaaf5466936
kalpit vyas <kalpitvyas0716@gmail.com>
2022-03-12 12:04:36 +0530

    Addition Program in Java in newjavabranch

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition{
+  public static void main(String [] args){
+	int a = 10;
+	int b = 20;
+	int c = a+b;
+	System.out.println("Addition is : " + c);
+  } //end of main
+} //end of class
